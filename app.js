cfc70119d2ce3bd61cd0f372cd463c40a5067125
Herve KRASOWIAK <krasowiak.h@gmail.com>
2022-05-30 17:30:47 +0200

    mise en place du Js, modif css modif html

---
+++ app.js
@@ -0,0 +1,7 @@
+// Evaluation de niveau individuel en Esapagnole.
+// Validation de chaque questions en cliquant sur le bouton reponse que l'on estime correcte.
+// Le résultat affiché corespond au nombre de bonnes réponses sur 10 questions.
+// Une fois le questionnaire terminé le soumettre afin de transmettre le résultat.
+
+const VALIDATION = "reponse";
+let soumettre = "soumettre";
